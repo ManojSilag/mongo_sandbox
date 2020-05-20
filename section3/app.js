71f572abf42ebb99c08fecf66b889d02b19ddf49
ManojSilag <manojsilag@gmail.com>
2020-05-20 13:24:43 +0530

    driver create

--- section3/app.js
+++ section3/app.js
@@ -1,7 +1,14 @@
 const express = require("express");
 const app = express();
 const routes = require("./routes/routes");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
 
+
+// mongoose.Promise = global.Promise;
+mongoose.connect("mongodb://localhost:27017/olaa", { useNewUrlParser: true });
+
+app.use(bodyParser.json());
 routes(app);
 
 module.exports = app;
--- section3/routes/routes.js
+++ section3/routes/routes.js
@@ -2,4 +2,7 @@ const DriverController = require("../controller.js/driver");
 
 module.exports = (app) => {
   app.get("/api", DriverController.greeting);
+
+  app.post("/api/drivers", DriverController.create)
+
 };
